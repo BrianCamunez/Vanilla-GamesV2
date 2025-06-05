0b7c57efb85e33f496193687b9ee2f9bb65493bd
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-06-05 19:37:05 +0200

    Configuracion de vite, instalación de dependencias y despliegue con GitHub Pages

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import { header } from './components/header'
+import { footer } from './components/footer'
+
+// Import all of Bootstrap's JS
+import 'bootstrap'
+
+// Import our custom CSS
+import './scss/styles.scss'
+
+
+// Importamos la vista por defecto (que será home)
+async function cargarVista () {
+  const componente = await import('./views/homeVista')
+  const vista = componente.default
+  // Inyectamos la vista home
+  document.querySelector('main').innerHTML = vista.template
+}
+cargarVista()
+
+//Inyectamos el componente header
+document.querySelector('header').innerHTML = header.template
+
+//Inyectamos el componente footer
+document.querySelector('footer').innerHTML = footer.template
